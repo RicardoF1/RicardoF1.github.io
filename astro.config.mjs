8e5b6c8c4a9f1a91121180dbb0cb71f0fca251d3
RicardoF1 <149124834+RicardoF1@users.noreply.github.com>
2025-03-30 13:57:44 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,12 @@ import { template } from "./src/settings";
 
 import sitemap from "@astrojs/sitemap";
 
+//new
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), tailwind(), sitemap()],
+    integrations: [mdx(), react(), tailwind(), sitemap()],
     site: template.website_url,
     base: template.base,
 });
